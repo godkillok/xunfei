c7a4eb5f736842f46ab3a0037e856a60515693d6
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2019-07-07 01:04:52 +0800

    tgp

--- class_base/common.py
+++ class_base/common.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 project_path="/data/tanggp/xfyun/classify/aichallenge"
 train_path=os.path.join(project_path,"apptype_train.dat")
@@ -7,6 +8,54 @@ import  jieba
 import json
 import re
 from collections import defaultdict
+import time
+import urllib.request
+token_url = "https://openapi.baidu.com/oauth/2.0/token?grant_type=client_credentials&client_id=%s&client_secret=%s"
+# 1.获取token
+api_key = 'hGs3TEt3sN3XcI3VyIAyuTQp'
+api_secert = 'P7tCqnwMBEPs6bpEa4TOr4voTtAtTdxQ'
+
+token_url = token_url % (api_key, api_secert)
+
+r_str = urllib.request.urlopen(token_url).read()
+r_str = str(r_str, encoding="utf-8")
+token_data = json.loads(r_str)
+token_str = token_data['access_token']
+
+url_all = 'https://aip.baidubce.com/rpc/2.0/nlp/v1/lexer?access_token=' + str(token_str)
+fs=['ai_challenger_oqmrc_trainingset.json','ai_challenger_oqmrc_testa.json','ai_challenger_oqmrc_validationset.json']
+
+def  segm(word1):
+
+    data2 = {'text': word1}
+    post_data = json.dumps(data2)
+    header_dict = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',
+                   'Content-Type': 'application/json'}
+    req = urllib.request.Request(url=url_all, data=bytes(post_data, encoding="gbk"), headers=header_dict)
+    res = urllib.request.urlopen(req).read()
+    # print(res)
+    r_data = str(res, encoding="GBK")
+    # print(r_data)
+    res = json.loads(r_data)
+
+    seg = []
+    for i in res.get('items'):
+        seg.append(i.get('item'))
+    sentence = ' '.join(seg)
+    return str(sentence)
+
+def  segment(text):
+    retry=3
+    while retry > 0:
+        try:
+            text= segm(text)
+            retry = -1
+        except Exception as e:
+            print(e)
+            time.sleep(1)
+    return text
+
+
 id_dic={}
 with open(id_path,"r",encoding="utf8") as f:
     lines=f.readlines()
@@ -36,6 +85,7 @@ for path in [train_path,test_path]:
             text2 = re.sub(r'[{}]+'.format(punctuation), ' ', str(text))
             seg_list=jieba.cut(text2.lower(), cut_all=False)
             res["jieba"]=' '.join(seg_list)
+            res["baidu"]=segment(text)
             if "train" in path:
                 llid=li[1]
                 if len(llid.split("|"))>2:
