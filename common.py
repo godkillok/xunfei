40c871bee5518b226afda5f5e705f7fcdd7a2284
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2019-07-06 19:53:46 +0800

    tgp

---
+++ common.py
@@ -0,0 +1,38 @@
+import os
+project_path="/data/tanggp/xfyun/classify/aichallenge"
+train_path=os.path.join(project_path,"apptype_train.dat")
+test_path=os.path.join(project_path,"app_desc.dat")
+id_path=os.path.join(project_path,"apptype_id_name.txt")
+import  jieba
+import json
+id_dic={}
+with open(id_path,"r",encoding="utf8") as f:
+    lines=f.readlines()
+    for li in lines:
+        li=li.strip()
+        lid,lname=li.split()
+        id_dic[lid]=lname
+
+for path in [train_path,test_path]:
+    result=[]
+    with open(path,"r",encoding="utf8") as f:
+        lines=f.readlines()
+        for li  in lines:
+            li=li.strip()
+            li=li.split()
+            res={}
+            res["app"]=li[0]
+            res["text"] = li[2]
+            seg_list=jieba.cut(li[2].lower(), cut_all=False)
+            res["jieba"]=' '.join(seg_list)
+            if "train" in path:
+                lid=li[1]
+                res["label"] = lid
+                res["label_name"] = id_dic[lid[:5]]+'#'+id_dic[lid]
+                res["label_1st"] = id_dic[lid[:5]]
+                res["label_2st"] = id_dic[lid]
+            result.append(res)
+
+    with open(path.replace("dat","jieba_json"), "w", encoding="utf8") as f:
+       for  res in result:
+           f.writelines(json.dumps(res)+'\n')
