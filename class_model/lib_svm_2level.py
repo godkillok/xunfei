1ba29d47f959588f460898df5b4e529b0089dc60
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2019-07-07 11:40:19 +0800

    tgp

--- class_model/lib_svm_2level.py
+++ class_model/lib_svm_2level.py
@@ -120,11 +120,13 @@ def svm_train():
         dic_lab[v]=k
     test_preds = []
     for l1,l2 in zip(test_preds_1,test_preds_2):
+        print(l2)
         dg=list(l2.argsort()[-2:][::-1])
         for i in range(l1.shape[0]):
             sc1=l1[0]
             for lv2 in label1_label2[i]:
                 l2[lv2]+=10*sc1
+        print(l2)
         dg2 = list(l2.argsort()[-2:][::-1])
         for d1,d2 in zip(dg,dg2):
             if d1!=d2:
