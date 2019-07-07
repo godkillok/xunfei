545c5c0058b7352f5b1189966063e91ab7319e42
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2019-07-08 00:37:02 +0800

    tgp

--- class_base/add_data.py
+++ class_base/add_data.py
@@ -48,3 +48,5 @@ print(lab_count)
 with open(train_path, "w", encoding="utf8") as f:
    for  res in result:
        f.writelines(json.dumps(res,ensure_ascii=False)+'\n')
+os.system("cd /data/tanggp/xfyun/classify/aichallenge && mv apptype_train.train_jieba_json apptype_train.train_jieba_json1")
+os.system("cd /data/tanggp/xfyun/classify/aichallenge && cat apptype_train.train_jieba_json1 new_train.jieba_json >apptype_train.train_jieba_json")
