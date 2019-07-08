606c029a61737989da53b3879bbd4ebb5af82f5c
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2019-07-08 23:28:18 +0800

    tgp

--- class_model/blend.py
+++ class_model/blend.py
@@ -66,7 +66,8 @@ if __name__ == '__main__':
         train_x = train_x[idx]
         train_y = train_y[idx]
 
-    skf = StratifiedKFold( n_folds)
+    sfolder = StratifiedKFold( n_folds)
+    skf =sfolder.split(train_x, train_y)
     lin_clf = CalibratedClassifierCV(svm.LinearSVC(C=0.1))
     lin_clf_1 = CalibratedClassifierCV(svm.LinearSVC(C=0.2))
     lin_clf_2 = CalibratedClassifierCV(svm.LinearSVC(C=0.5))
