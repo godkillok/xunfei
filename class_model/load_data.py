5d796f0bb029336d51b07d1686e1058a8a3ba14e
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2019-07-09 20:36:15 +0800

    tgp

--- class_model/load_data.py
+++ class_model/load_data.py
@@ -36,9 +36,10 @@ def get_data_set(flie):
         lines_json.append(li)
 
     need_repeat={}
+    num=100
     for k,v in lab_count.items():
-        if v<35:
-            need_repeat[k]=int(35/v)+1
+        if v<100:
+            need_repeat[k]=int(100/v)+1
 
     if flie==train_path:
         for li in lines:
