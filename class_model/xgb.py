75e7a021c2edebb054696fd6ba3bd02c7fddec4e
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2019-07-10 00:37:48 +0800

    tgp

--- class_model/xgb.py
+++ class_model/xgb.py
@@ -88,3 +88,16 @@ predDf.columns = predcol
 lr_clf = LogisticRegression(random_state=0, solver='sag',multi_class='ovr', verbose = 1)
 
 lr_clf.fit(trainDf, y_train)
+
+train_preds = lin_clf.predict(trainDf)
+
+from sklearn.metrics import classification_report
+
+logging.info('train {} accuracy_score {},  \n {}'.format('train', accuracy_score(train_y, train_preds),
+                                                         classification_report(train_y, train_preds)))
+
+
+test_preds = lin_clf.predict(testDf)
+
+logging.info('train {} accuracy_score {},  \n {}'.format('test', accuracy_score(test_y, test_preds),
+                                                         classification_report(test_y, test_preds)))
