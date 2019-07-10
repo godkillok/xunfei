47ed58919e20b6e44f6e4afea8a31bf53864f4c1
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2019-07-10 14:42:46 +0800

    tgp

--- class_model/sh_hyper4.sh
+++ class_model/sh_hyper4.sh
@@ -1,3 +1,3 @@
 rm -rf /data/tanggp/xunfei/class_model/log_hyper.log
-cd /data/tanggp/xunfei/class_model/ &&  python3 hyper_2.py  >>/data/tanggp/xunfei/class_model/log_hyper3.log 2>&1
+cd /data/tanggp/xunfei/class_model/ &&  python3 hyper_2.py  >>/data/tanggp/xunfei/class_model/log_hyper4.log 2>&1
 
