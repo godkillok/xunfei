1931ce7e377216c995cfaf7fd9049940ed72ca15
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2019-07-11 21:53:50 +0800

    tgp

--- class_base/tengxun.py
+++ class_base/tengxun.py
@@ -4,6 +4,7 @@ project_path="/data/tanggp/xfyun/classify/aichallenge"
 test_path=os.path.join(project_path,"apptype_train.test_jieba_json")
 train_path=os.path.join(project_path,"apptype_train.train_jieba_json1")
 pred_path=os.path.join(project_path,"app_desc.jieba_json")
+embed_path=os.path.join(project_path,"tencent")
 pre_train=os.path.join("/data/tanggp","Tencent_AILab_ChineseEmbedding.txt")
 word_list=[]
 for fi in [test_path,train_path,pred_path]:
@@ -17,8 +18,11 @@ for fi in [test_path,train_path,pred_path]:
 word_list=set(word_list)
 print(word_list[:10])
 embed=[]
-with open("","r",encoding="utf8") as f:
+with open(pre_train,"r",encoding="utf8") as f:
     for li in f.readline():
         word=li.split()[0]
         if word in word_list:
             embed.append(li)
+with open(embed_path,"r",encoding="utf8") as f:
+    for em in embed:
+        f.writelines(em)
