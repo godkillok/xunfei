921e8643a29d65e45f5a0827a5f19082e0f0363e
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2019-07-11 21:52:15 +0800

    tgp

---
+++ class_base/tengxun.py
@@ -0,0 +1,24 @@
+import os
+import json
+project_path="/data/tanggp/xfyun/classify/aichallenge"
+test_path=os.path.join(project_path,"apptype_train.test_jieba_json")
+train_path=os.path.join(project_path,"apptype_train.train_jieba_json1")
+pred_path=os.path.join(project_path,"app_desc.jieba_json")
+pre_train=os.path.join("/data/tanggp","Tencent_AILab_ChineseEmbedding.txt")
+word_list=[]
+for fi in [test_path,train_path,pred_path]:
+    with open(fi,"r",encoding="utf8") as f:
+        lines=f.readlines()
+        for li in lines:
+            li=json.loads(li)
+            jieba=li.get("jieba")
+            word_list+=jieba.split()
+
+word_list=set(word_list)
+print(word_list[:10])
+embed=[]
+with open("","r",encoding="utf8") as f:
+    for li in f.readline():
+        word=li.split()[0]
+        if word in word_list:
+            embed.append(li)
