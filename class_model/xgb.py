984198e0a6d858a468eb190509338d24cbedd757
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2019-07-11 11:41:54 +0800

    tgp

--- class_model/xgb.py
+++ class_model/xgb.py
@@ -102,23 +102,23 @@ logging.info("bulid feature done trainDf.shape {}".format(trainDf.shape))
 trainDf.columns = traincol
 testDf.columns = testcol
 predDf.columns = predcol
-lr_clf = LogisticRegression(random_state=0, solver='saga',multi_class='ovr', max_iter=1000,verbose =False ,n_jobs=-1)
-
-lr_clf.fit(trainDf, train_y)
-
-train_preds = lr_clf.predict(trainDf)
-
-from sklearn.metrics import classification_report
-
-logging.info('train {} accuracy_score {},  \n {}'.format('train', accuracy_score(train_y, train_preds),
-                                                         classification_report(train_y, train_preds)))
-
-
-test_preds = lr_clf.predict(testDf)
-
-logging.info('train {} accuracy_score {},  \n {}'.format('test', accuracy_score(test_y, test_preds),
-                                                         classification_report(test_y, test_preds)))
-
+# lr_clf = LogisticRegression(random_state=0, solver='saga',multi_class='ovr', max_iter=1000,verbose =False ,n_jobs=-1)
+#
+# lr_clf.fit(trainDf, train_y)
+#
+# train_preds = lr_clf.predict(trainDf)
+#
+# from sklearn.metrics import classification_report
+#
+# logging.info('train {} accuracy_score {},  \n {}'.format('train', accuracy_score(train_y, train_preds),
+#                                                          classification_report(train_y, train_preds)))
+#
+#
+# test_preds = lr_clf.predict(testDf)
+#
+# logging.info('train {} accuracy_score {},  \n {}'.format('test', accuracy_score(test_y, test_preds),
+#                                                          classification_report(test_y, test_preds)))
+#
 
 parms = {'task': 'train',
     'boosting_type': 'gbdt',
