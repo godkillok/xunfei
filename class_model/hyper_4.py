77420b3f41a1032494b8eb41cb0d3fc75bec4b51
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2019-07-12 23:53:32 +0800

    tgp

--- class_model/hyper_4.py
+++ class_model/hyper_4.py
@@ -79,7 +79,7 @@ def score(params):
         lin_clf = CalibratedClassifierCV(lin_clf)
         lin_clf.fit(trn_term_doc, train_y)
         test_preds_prob = lin_clf.predict_proba(test_term_doc)
-        test_y2, test_preds = top_2_label_code(test_preds_prob, test_y[:])
+        test_y2, test_preds = top_2_label_code(test_preds_prob, test_y)
 
         acc3 = accuracy_score(test_y2, test_preds)
 
