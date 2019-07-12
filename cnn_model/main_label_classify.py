700de8ab6310ac4057480568cc156d3e672c57ec
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2019-07-13 01:45:22 +0800

    tgp

--- cnn_model/main_label_classify.py
+++ cnn_model/main_label_classify.py
@@ -8,7 +8,7 @@ from train_model import model_fn
 import json
 import logging
 #from best_checkpoint_copier import BestCheckpointCopier
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix,accuracy_score
 from logger import get_logger
 log_file_name = os.path.basename(__file__).split('.', 1)[0] + '.log'
 # Save params
@@ -61,14 +61,14 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # 只显示warning 和 error
 tf.logging.set_verbosity(logging.INFO)
 
 
-timestamp = time.strftime("%m_%d_%H", time.localtime())
-if FLAGS.do_train:
-    model_dir = os.path.join(FLAGS.model_dir)
-    with open('model_dir.pkl', 'wb') as f:
-        pickle.dump(model_dir, f)
-else:
-    with open('model_dir.pkl', 'rb') as f:
-        model_dir = pickle.load(f)
+# timestamp = time.strftime("%m_%d_%H", time.localtime())
+# if FLAGS.do_train:
+#     model_dir = os.path.join(FLAGS.model_dir)
+#     with open('model_dir.pkl', 'wb') as f:
+#         pickle.dump(model_dir, f)
+# else:
+#     with open('model_dir.pkl', 'rb') as f:
+#         model_dir = pickle.load(f)
 
 def input_fn(filenames, config, shuffle_buffer_size):
     def parser(record):
@@ -94,16 +94,33 @@ def id_word_map():
         vocab_dict = {str(i):l.strip() for i, l in enumerate(lines)}
     return vocab_dict
 
-if __name__ == '__main__':
+def top_2_label_code(test_preds_prob,test_y):
+    test_preds = []
+    for prob in test_preds_prob:
+        test_preds.append(list(prob.argsort()[-2:][::-1]))
+
+    test_y_name = []
+    test_preds_code= []
+    for real, pred in zip(test_y, test_preds):
+        prd = pred[0]
+        # print(real, pred)
+        for pr in pred:
+            if real == pr:
+                prd = real
+        test_y_name.append(real)
+        test_preds_code.append(prd)
+    return test_y_name,test_preds_code
+
+def main_class():
     start = time.time()
     # Loads parameters from json file
-    vocab_dict=id_word_map()
+    vocab_dict = id_word_map()
     with open(FLAGS.params_file) as f:
         config = json.load(f)
-    #config["train_size"]=9722067
-    config["max_length"]=250
-    config["id_word"]=vocab_dict
-    config["word_dim"]=128
+    # config["train_size"]=9722067
+    config["max_length"] = 250
+    config["id_word"] = vocab_dict
+    config["word_dim"] = 128
     if config["train_size"] < FLAGS.shuffle_buffer_size:
         FLAGS.shuffle_buffer_size = config["train_size"]
 
@@ -112,7 +129,8 @@ if __name__ == '__main__':
     session_config = tf.ConfigProto(log_device_placement=True)
     session_config.gpu_options.per_process_gpu_memory_fraction = 0.7
     session_config.gpu_options.allow_growth = True
-    run_config = tf.estimator.RunConfig(save_checkpoints_steps=FLAGS.steps_check, session_config=session_config, keep_checkpoint_max=3)
+    run_config = tf.estimator.RunConfig(save_checkpoints_steps=FLAGS.steps_check, session_config=session_config,
+                                        keep_checkpoint_max=3)
     num_warmup_steps = int(train_steps * FLAGS.warmup_proportion)
     early_stop_steps = int(train_steps * 0.4)
     estimator = tf.estimator.Estimator(
@@ -120,9 +138,9 @@ if __name__ == '__main__':
         model_dir=model_dir,
         config=run_config,
         params={
-            "word_dim":config["word_dim"],
-            "id_word":config["id_word"],
-            "train_size":config["train_size"],
+            "word_dim": config["word_dim"],
+            "id_word": config["id_word"],
+            "train_size": config["train_size"],
             'max_length': config["max_length"],
             'emb_file': FLAGS.emb_file,
             'learning_rate': FLAGS.learning_rate,
@@ -130,13 +148,13 @@ if __name__ == '__main__':
             'dropout_prob': FLAGS.dropout_prob,
             'word_dim': FLAGS.word_dim,
             'vocab': FLAGS.word_path,
-            'num_filters':FLAGS.num_filters,
+            'num_filters': FLAGS.num_filters,
             'filter_sizes': list(map(int, FLAGS.filter_sizes.split(","))),
             'num_warmup_steps': num_warmup_steps,
             'train_steps': train_steps,
-            'summary_dir':model_dir,
-            "label_size":125,
-            'use_focal_loss':False,
+            'summary_dir': model_dir,
+            "label_size": 125,
+            'use_focal_loss': False,
             'use_author_feature': False,
             'use_category_feature': False,
             'use_keyword_feature': False,
@@ -146,7 +164,11 @@ if __name__ == '__main__':
     no_increase_steps = int(config["train_size"] / FLAGS.batch_size * FLAGS.early_stop_epoches)
 
     # 用于early stop
-    early_stop_hook = tf.contrib.estimator.stop_if_no_increase_hook(estimator, metric_name='f1', max_steps_without_increase=no_increase_steps, min_steps=early_stop_steps, run_every_secs=120)
+    early_stop_hook = tf.contrib.estimator.stop_if_no_increase_hook(estimator, metric_name='f1',
+                                                                    max_steps_without_increase=no_increase_steps,
+                                                                   min_steps=early_stop_steps, run_every_secs=120)
+    acc2=0
+
     # timeline_hook = tf.train.ProfilerHook(save_steps=FLAGS.steps_check, output_dir=model_dir + '/timeline/')
     if FLAGS.do_train == True:
         input_fn_for_train = lambda: input_fn(FLAGS.train_file, config, FLAGS.shuffle_buffer_size)
@@ -157,7 +179,7 @@ if __name__ == '__main__':
         #         score_metric='acc',  # metric to use to determine "best"
         #         compare_fn=lambda x, y: x.score > y.score,
         #         sort_reverse=True)
-        eval_spec = tf.estimator.EvalSpec(input_fn=input_fn_for_eval, throttle_secs=1200) # exporters=best_copier
+        eval_spec = tf.estimator.EvalSpec(input_fn=input_fn_for_eval, throttle_secs=1200)  # exporters=best_copier
         tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
         logger.info("Switch to the current directory and Run the command line:" \
                     "tensorboard --logdir=%s" \
@@ -166,26 +188,39 @@ if __name__ == '__main__':
     if FLAGS.do_predict == True:
         best_dir = model_dir + '/best'
 
-        path_label=os.path.join(FLAGS.data_dir, 'textcnn_label_sort')
+        path_label = os.path.join(FLAGS.data_dir, 'textcnn_label_sort')
         with open(path_label, 'r', encoding='utf8') as f:
             lines = f.readlines()
-            id2label = { i:l.strip().split("\x01\t")[0] for i, l in enumerate(lines)}
+            id2label = {i: l.strip().split("\x01\t")[0] for i, l in enumerate(lines)}
 
         # predict
         predict_label_list = []
         true_label_list = []
+        prob_list=[]
+        true_label_code=[]
         input_fn_for_test = lambda: input_fn(FLAGS.valid_file, config, 0)
         output_results = estimator.predict(input_fn_for_test, checkpoint_path=tf.train.latest_checkpoint(best_dir))
+
         with open(FLAGS.result_file, 'w') as writer:
-             for prediction in output_results:
-                 predict_label_id = prediction["predict_label_ids"]
-                 true_label_id = prediction["true_label_ids"]
-                 predict_label = id2label[predict_label_id]
-                 true_label = id2label[true_label_id]
-                 predict_label_list.append(predict_label)
-                 true_label_list.append(true_label)
-                 writer.write(predict_label + '\t' + true_label + "\n")
+            for prediction in output_results:
+                predict_label_id = prediction["predict_label_ids"]
+                true_label_id = prediction["true_label_ids"]
+                prob_list.append(prediction["probabilities"])
+                predict_label = id2label[predict_label_id]
+                true_label = id2label[true_label_id]
+                predict_label_list.append(predict_label)
+                true_label_code.append(true_label_id)
+                true_label_list.append(true_label)
+                writer.write(predict_label + '\t' + true_label + "\n")
+        test_y_name, test_preds_code=top_2_label_code(prob_list, true_label_code)
+        acc2=accuracy_score( test_y_name, test_preds_code)
         logger.info(best_dir)
+
         logger.info(classification_report(true_label_list, predict_label_list))
-    elapsed_time = (time.time() - start)/60/60
-    logger.info("The total program takes {} hours".format(elapsed_time))
+    elapsed_time = (time.time() - start) / 60 / 60
+    logger.info("The total program takes {} hours and top2 acc is {}".format(elapsed_time,acc2))
+    return acc2
+
+
+if __name__ == '__main__':
+    main_class()
