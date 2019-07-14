5ee1709a0136bb5300d95befb3ef3d3ea9a00fc5
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2019-07-14 08:44:10 +0800

    tgp

--- cnn_model/post_pred.py
+++ cnn_model/post_pred.py
@@ -20,7 +20,7 @@ def top_2_label_code(test_preds_prob,test_y):
     return test_y_name,test_preds_code
 
 
-def post_pred(path_label,model_dir,history_dir,output_results):
+def post_eval(path_label,model_dir,history_dir,output_results):
     with open(path_label, 'r', encoding='utf8') as f:
         lines = f.readlines()
         id2label = {i: l.strip().split("\x01\t")[0] for i, l in enumerate(lines)}
@@ -49,18 +49,45 @@ def post_pred(path_label,model_dir,history_dir,output_results):
     logging.info(
     "The total program acc1 {} and top2 acc is {}".format(acc1, acc2))
     root, folder_name = os.path.split(model_dir)
-    if acc2 > 0.7:
-        cmd = "cd {} && mv {} model_{}".format(root,folder_name, acc2)
-        output_eval_file = os.path.join(history_dir, "p_{}_results.txt".format(acc2))
-        try:
-            os.makedirs(history_dir)
-        except:
-            pass
-        with tf.gfile.GFile(output_eval_file, "a") as writer:
-            for guid, prob in zip(guids, prob_list):
-                writer.write('{},{} \n'.format(guid, ','.join([str(pr) for pr in prob])))
-    else:
+    if acc2 <= 0.7:
         cmd = "cd {} && rm -rf {}".format(root,folder_name)
-    logging.info("==========")
-    logging.info(cmd)
-    os.system(cmd)
+        logging.info("==========")
+        logging.info(cmd)
+        os.system(cmd)
+    return acc2
+
+def post_pred(path_label,model_dir,history_dir,output_results,acc2):
+    with open(path_label, 'r', encoding='utf8') as f:
+        lines = f.readlines()
+        id2label = {i: l.strip().split("\x01\t")[0] for i, l in enumerate(lines)}
+
+    # predict
+    predict_label_list = []
+    true_label_list = []
+    prob_list = []
+    true_label_code = []
+    guids = []
+
+    for prediction in output_results:
+        predict_label_id = prediction["predict_label_ids"]
+        prob_list.append(prediction["probabilities"])
+        guids.append(prediction["guid"])
+        predict_label = id2label[predict_label_id]
+
+        predict_label_list.append(predict_label)
+
+    root, folder_name = os.path.split(model_dir)
+
+    output_eval_file = os.path.join(history_dir, "p_{}_results.txt".format(acc2))
+    try:
+        os.makedirs(history_dir)
+    except:
+        pass
+    if acc2 > 0.7:
+        cmd = "cd {} && mv {} model_{}".format(root, folder_name, acc2)
+        logging.info("==========")
+        logging.info(cmd)
+        os.system(cmd)
+    with tf.gfile.GFile(output_eval_file, "w") as writer:
+        for guid, prob in zip(guids, prob_list):
+            writer.write('{},{} \n'.format(guid, ','.join([str(pr) for pr in prob])))
