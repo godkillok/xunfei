2fcb9ec585e73fb55c8a73046b30f3d9c6c59bb2
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2019-07-14 10:57:17 +0800

    tgp

--- bert/data_prepare.py
+++ bert/data_prepare.py
@@ -458,9 +458,8 @@ def main(_):
 
         train_examples = processor.get_train_examples(FLAGS.data_dir)
         num_train_example = len(train_examples)
-        num_train_steps = int(
-            num_train_example / FLAGS.train_batch_size * FLAGS.num_train_epochs)
-        num_warmup_steps = int(num_train_steps * FLAGS.warmup_proportion)
+        num_train_steps = None
+        num_warmup_steps =None
         d = {
             'num_train_example': num_train_example,
             'num_train_steps': num_train_steps,
