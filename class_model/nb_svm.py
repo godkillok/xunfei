049dc6b3e712151c7574d9153b5b0477e6f40a94
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2019-07-16 12:46:19 +0800

    tgp

--- class_model/nb_svm.py
+++ class_model/nb_svm.py
@@ -90,7 +90,7 @@ for i, j in enumerate(TARGET_COLS):
 
     try:
         valid_loss_class=log_loss(y_valid,preds_valid[:,i])
-        acc = top_2_label_code(y_valid,model.predict_proba(trn_term_doc))
+        acc = top_2_label_code(model.predict_proba(trn_term_doc),y_valid)
     except  Exception as e:
         print(e)
         valid_loss_class= np.mean(train_loss)
