bfe5f4013d452c9b7018b7112ccefb9a2b683867
tanggp@192.168.5.250:29418 <tanggp@ushareit.com>
2019-07-16 23:35:00 +0800

    tgp

---
+++ bert/run_feature.sh
@@ -0,0 +1,15 @@
+#export BERT_BASE_DIR=/data/tanggp/bert/multi_cased_L-12_H-768_A-12
+export BERT_BASE_DIR=/data/tanggp/bert/chinese_L-12_H-768_A-12
+export INPUT=/data/tanggp/xun_class/aichallenge
+export OUTPUT=/data/tanggp/xun_class/bert_embedding/
+#123
+cp /data/tanggp/bert/chinese_L-12_H-768_A-12/bert_config.json /data/tanggp/xun_class/bert_model_mul/
+cp /data/tanggp/bert/chinese_L-12_H-768_A-12/vocab.txt /data/tanggp/xun_class/bert_model_mul/
+python3 run_classify.py \
+  --task_name=category \
+  --input_file=$OINPUT/apptype_train.train_embedding\
+  --output_file=$OUTPUT/apptype_train.train_jieba_json1\
+  --vocab_file=$BERT_BASE_DIR/vocab.txt \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+  --max_seq_length=200
